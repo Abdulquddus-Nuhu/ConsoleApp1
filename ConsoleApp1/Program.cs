437cc0f5107a232318a54920eb3416c6f3411654
Abdulquddus Nuhu <abdulquddusnuhu@gmail.com>
2024-04-29 11:12:15 +0100

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+const string name = "World";
+const string namess = "World";
+const string namse = "World";
